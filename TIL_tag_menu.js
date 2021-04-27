f4ce5819b3edc12441dda3f2a9e45f337614ce46
hagus99 <hagus99@gmail.com>
2021-04-28 00:58:12 +0900

    4/28

---
+++ TIL_tag_menu.js
@@ -0,0 +1,8 @@
+
+let menu = document.getElementById('tag_menu');
+//create new div element
+let C = document.createElement('div');
+C.textContent = 'C언어';
+menu.appendChild(C);
+
+console.log(menu.innerHTML);
