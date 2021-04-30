681c9b9326fb43529b42a9c58f2b25ddb9ec6ab8
hagus99 <hagus99@gmail.com>
2021-04-30 15:02:28 +0900

    4/30

--- TIL_tag_menu.js
+++ TIL_tag_menu.js
@@ -2,7 +2,6 @@
 let menu = document.getElementById('tag_menu');
 //create new div element
 let C = document.createElement('div');
-C.textContent = 'C언어';
+C.textContent = 'C_language';
 menu.appendChild(C);
-
 console.log(menu.innerHTML);
--- tag_menu.js
+++ tag_menu.js
@@ -5,4 +5,9 @@ let movies = document.createElement('div');
 movies.textContent = 'Movies';
 menu.appendChild(movies);
 
+
+let travel = document.createElement('div');
+travel.textContent = 'Travel';
+menu.appendChild(travel);
+
 console.log(menu.innerHTML);
