c9c09c86cfab07a9305457816c25c648c53e5587
hagus99 <hagus99@gmail.com>
2021-05-07 01:07:18 +0900

    5/6

--- TIL_tag_menu.js
+++ TIL_tag_menu.js
@@ -1,7 +1,22 @@
 
 let menu = document.getElementById('tag_menu');
 //create new div element
-let C = document.createElement('div');
-C.textContent = 'C_language';
-menu.appendChild(C);
+let Bible = document.createElement('div');
+Bible.textContent = 'Bible';
+menu.appendChild(Bible);
+
+let Class = document.createElement('div');
+Class.textContent = 'Class';
+menu.appendChild(Class);
+
+let Study = document.createElement('div');
+Study.textContent = 'Study';
+menu.appendChild(Study);
+
+let Language = document.createElement('div');
+Language.textContent = 'Language';
+menu.appendChild(Language);
+
+
+
 console.log(menu.innerHTML);
--- tag_menu.js
+++ tag_menu.js
@@ -1,6 +1,26 @@
+function loadJQuery() {
+    var oScript = document.createElement("script");
+    oScript.type = "text/javascript";
+    oScript.charset = "utf-8";		  
+    oScript.src = "http://code.jquery.com/jquery-1.6.2.min.js";	
+    document.getElementsByTagName("head")[0].appendChild(oScript);
+}
+
 
 let menu = document.getElementById('tag_menu');
 //create new div element
+let history = document.createElement('div');
+history.textContent = 'History';
+menu.appendChild(history);
+
+let Playlist = document.createElement('div');
+Playlist.textContent = 'Playlist';
+menu.appendChild(Playlist);
+
+let Booklist = document.createElement('div');
+Booklist.textContent = 'Booklist';
+menu.appendChild(Booklist);
+
 let movies = document.createElement('div');
 movies.textContent = 'Movies';
 menu.appendChild(movies);
@@ -11,3 +31,4 @@ travel.textContent = 'Travel';
 menu.appendChild(travel);
 
 console.log(menu.innerHTML);
+
