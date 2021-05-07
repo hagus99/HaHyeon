57d6b65fdfc19be22e17993d29ed23b33d40e50a
hagus99 <hagus99@gmail.com>
2021-05-08 05:29:42 +0900

    5/8

--- TIL_tag_menu.js
+++ TIL_tag_menu.js
@@ -2,15 +2,21 @@
 let menu = document.getElementById('tag_menu');
 //create new div element
 
-let Class = document.createElement('div');
+let Class = document.createElement('button');
 Class.textContent = 'Class';
+Class.addEventListener('click',MClass);
+
+function MClass(event){
+    window.location
+}
+
 menu.appendChild(Class);
 
-let Study = document.createElement('div');
+let Study = document.createElement('button');
 Study.textContent = 'Study';
 menu.appendChild(Study);
 
-let Language = document.createElement('div');
+let Language = document.createElement('button');
 Language.textContent = 'Language';
 menu.appendChild(Language);
 
--- tag_menu.js
+++ tag_menu.js
@@ -9,29 +9,73 @@ function loadJQuery() {
 
 let menu = document.getElementById('tag_menu');
 //create new div element
-let history = document.createElement('div');
+let history = document.createElement('button');
 history.textContent = 'History';
+
+history.addEventListener('click',Mhistory);
+
+function Mhistory(event){
+    window.location.href = 'https://hagus99.github.io/HaHyeon/tag.html'
+}
 menu.appendChild(history);
 
-let Bible = document.createElement('div');
+
+
+let Bible = document.createElement('button');
 Bible.textContent = 'Bible';
+
+Bible.addEventListener('click',Mbible);
+
+function Mbible(event){
+    window.location.href = '#bible'
+}
+
 menu.appendChild(Bible);
 
-let Playlist = document.createElement('div');
+let Playlist = document.createElement('button');
 Playlist.textContent = 'Playlist';
+
+Playlist.addEventListener('click',MPlaylist);
+
+function MPlaylist(event){
+    window.location.href = '#playlist'
+}
+
 menu.appendChild(Playlist);
 
-let Booklist = document.createElement('div');
+let Booklist = document.createElement('button');
 Booklist.textContent = 'Booklist';
+
+Booklist.addEventListener('click',Mbooklist);
+
+function Mbooklist(event){
+    window.location.href = '#booklist'
+}
 menu.appendChild(Booklist);
 
-let movies = document.createElement('div');
+
+
+let movies = document.createElement('button');
 movies.textContent = 'Movies';
+
+movies.addEventListener('click',Mmovies);
+
+function Mmovies(event){
+    window.location.href = '#movies'
+}
+
 menu.appendChild(movies);
 
 
-let travel = document.createElement('div');
+let travel = document.createElement('button');
 travel.textContent = 'Travel';
+
+travel.addEventListener('click',Mtravel);
+
+function Mtravel(event){
+    window.location.href = '#travel'
+}
+
 menu.appendChild(travel);
 
 console.log(menu.innerHTML);
