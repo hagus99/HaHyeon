94e4a9a63b444a64b64981d3fccebaa61daec4f5
hagus99 <hagus99@gmail.com>
2021-05-09 13:13:34 +0900

    5/9

--- TIL_tag_menu.js
+++ TIL_tag_menu.js
@@ -7,17 +7,30 @@ Class.textContent = 'Class';
 Class.addEventListener('click',MClass);
 
 function MClass(event){
-    window.location
+    window.location.href="file:///Users/hahyeon/Desktop/CHEESE/Hahyeon/TIL.html#class"
 }
 
 menu.appendChild(Class);
 
 let Study = document.createElement('button');
 Study.textContent = 'Study';
+
+Study.addEventListener('click',MStudy);
+
+function MStudy(event){
+    window.location.href="file:///Users/hahyeon/Desktop/CHEESE/Hahyeon/TIL.html#study"
+}
 menu.appendChild(Study);
 
 let Language = document.createElement('button');
 Language.textContent = 'Language';
+
+Language.addEventListener('click',MLanguage);
+
+function MLanguage(event){
+    window.location.href="file:///Users/hahyeon/Desktop/CHEESE/Hahyeon/TIL.html#language"
+}
+
 menu.appendChild(Language);
 
 
