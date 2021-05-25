f57f5c82b6351d2ecd5012cc9ab8c5c90e4e6013
hagus99 <hagus99@gmail.com>
2021-05-26 02:25:12 +0900

    5/26

--- MainMenu.js
+++ MainMenu.js
@@ -42,7 +42,7 @@ project.textContent = 'H.Project';
 topmenu.appendChild(project);
 project.addEventListener('click',Mproject);
 function Mproject(event){
-    window.location.href=""
+    window.location.href="";
 }
 
 
